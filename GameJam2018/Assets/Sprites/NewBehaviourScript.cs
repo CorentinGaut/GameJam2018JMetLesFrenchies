c593bfabede72bebc70d9e4c0b413eb693505847
Homère <homerebourgeois@hotmail.fr>
2018-03-02 23:34:54 +0100

    no message

--- GameJam2018/Assets/Sprites/NewBehaviourScript.cs
+++ GameJam2018/Assets/Sprites/NewBehaviourScript.cs
@@ -6,7 +6,7 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("jfjf");
+        Debug.Log("jfjfl");
 	}
 	
 	// Update is called once per frame
