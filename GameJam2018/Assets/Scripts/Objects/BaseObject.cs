d9240b257ff84ddc19eff27109083f9fde12cf13
Homère <homerebourgeois@hotmail.fr>
2018-03-03 00:38:02 +0100

    blub

---
+++ GameJam2018/Assets/Scripts/Objects/BaseObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseObject : MonoBehaviour {
+
+    public int HP;
+    public bool isRepared;
+    public int maxHP;
+    public float repareCooldown;
+    public BoxCollider repareBox;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected virtual void Repare()
+    {
+        HP = 0;
+        isRepared = false;
+    }
+
+    protected virtual void Destroy()
+    {
+        if (repareCooldown <= 0 && !isRepared)
+        {
+            HP += 10;
+            repareCooldown = 1;
+            if (HP == maxHP)
+                isRepared = true;
+        }
+    }
+}
---
+++ GameJam2018/Assets/Scripts/Objects/CPU.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPU : BaseObject {
+
+
+    // Use this for initialization
+    void Start () {
+        HP = 100;
+        maxHP = 200;
+        isRepared = false;
+        repareCooldown = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetButton("Jump"))
+        //    Repare();
+        //if (Input.GetButton("Fire1"))
+        //    Destroy();
+        if (repareCooldown > 0)
+            repareCooldown -= Time.deltaTime;
+	}
+
+    protected override void Destroy()
+    {
+        base.Destroy();
+    }
+
+    protected override void Repare()
+    {
+        base.Repare();
+    }
+}
