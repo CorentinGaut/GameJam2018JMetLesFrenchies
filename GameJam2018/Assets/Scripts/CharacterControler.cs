8d88ee9168dca13e21685fa6701b51d25f0757c2
Gautier <corentin.chignin@hotmail.fr>
2018-03-03 00:35:31 +0100

    Ajout deplacement

--- GameJam2018/Assets/Scripts/CharacterControler.cs
+++ GameJam2018/Assets/Scripts/CharacterControler.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class CharacterControler : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Vector3 direction;
+    // Use this for initialization
+    void Start()
+    {
+        direction = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+            direction.x = Input.GetAxis("Horizontal");
+            direction.z = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("ENFONCE LA");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (direction.magnitude > 0.1)
+        {
+            if (direction.magnitude > 1)
+                direction.Normalize();
+            this.transform.position += direction;     
+        }
+    }
 }
