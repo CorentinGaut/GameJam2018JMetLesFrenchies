6399b63a1eb9ff4fffa8bfe1e5d9b9bdd7078b7e
Homère <homerebourgeois@hotmail.fr>
2018-03-03 20:45:55 +0100

    particules reparation et destruction

---
+++ GameJam2018/Assets/Scripts/Objects/DestroyParticleEmitter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleEmitter : MonoBehaviour {
+
+
+    public ParticleSystem destroyedParticle;
+    public BoxCollider box;
+    // Use this for initialization
+    void Start()
+    {
+        var dsh = destroyedParticle.shape;
+        dsh.scale = box.size;
+        if (!GetComponentInParent<BaseObject>().isRepared)
+        {
+            var dem = destroyedParticle.emission;
+            dem.enabled = true;
+        }
+    }
+
+    public void StartEmitDestroyParticle()
+    {
+
+                var dem = destroyedParticle.emission;
+                dem.enabled = true; 
+        
+    }
+
+    public void StopEmitParticle()
+    {
+
+                var dem = destroyedParticle.emission;
+
+    }
+}
