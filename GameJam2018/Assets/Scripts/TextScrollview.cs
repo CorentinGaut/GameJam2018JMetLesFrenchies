96dbeae5e061583adaf23d0851a7dc414f3a2a9f
Lupusanghren <brisingr34130@gmail.com>
2018-03-02 19:51:54 -0500

    merge

--- GameJam2018/Assets/Scripts/TextScrollview.cs
+++ GameJam2018/Assets/Scripts/TextScrollview.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class TextScrollview : MonoBehaviour {
 
 	// Use this for initialization
+
+	public UnityEngine.UI.Text prefab;
+	private int compteur;
 	void Start () {
-		
+		compteur=0;
+		AddText("test");
+		AddText("test2");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	//Creer un objet text avec le text txt
+	void AddText(string txt){
+		//var clone = Instantiate(prefab,this.transform.position-this.transform.up,this.transform.rotation);
+		//var tmp = this.transform;
+		var tmp=prefab.rectTransform;
+		var tmpPos=tmp.position;
+		tmpPos.y-=14*compteur;
+		Debug.Log(tmpPos.y);
+		compteur++;
+		var clone = Instantiate(prefab,tmp);
+		var texte = clone.text;
+		texte+=txt+"_";
+		clone.text=texte;
 	}
 }
