aab3d85a44435cfd2e4b6a02bb37fb8540a306fe
Lupusanghren <brisingr34130@gmail.com>
2018-03-02 23:55:31 -0500

    Maj de l input

--- GameJam2018/Assets/Scripts/CharacterControler.cs
+++ GameJam2018/Assets/Scripts/CharacterControler.cs
@@ -26,29 +26,32 @@ public class CharacterControler : MonoBehaviour
             direction.x = Input.GetAxis("Horizontal");
             direction.z = Input.GetAxis("Vertical");
 
+        //a pour poser
+        //x pour reparer
+        //gachettes pour tourner un objet
 
         Camera.main.transform.position = gameObject.transform.position + new Vector3(0, 10, -10);
 
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetButtonDown("Repare"))
         {
             closestObject.GetComponent<BaseObject>().Repare();
             Debug.Log("ENFONCE LA");
         }
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetButtonDown("RotateD"))
         {
             closestObject.GetComponent<BaseObject>().Rotate(-45);
             Debug.Log("droite");
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetButtonDown("RotateG"))
         {
             closestObject.GetComponent<BaseObject>().Rotate(45);
             Debug.Log("gauche");
         }
 
 
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetButtonDown("Poser"))
         {
             PickUp();
             Debug.Log("gauche");
