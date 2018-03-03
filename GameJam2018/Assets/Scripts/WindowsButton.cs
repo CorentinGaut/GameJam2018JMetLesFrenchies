a7042052f9f893aef22c0cbd1186ba491cfa79f7
PC_STIEN\Justine <almes.justine@gmail.com>
2018-03-03 00:01:37 -0500

    Plein de truc cool ;)

--- GameJam2018/Assets/Scripts/WindowsButton.cs
+++ GameJam2018/Assets/Scripts/WindowsButton.cs
@@ -10,11 +10,20 @@ public class WindowsButton : MonoBehaviour {
     private UnityAction[] tabFonctions;
     private GameObject colomneDemarrer;
     private bool boolDemarrer;
+    public GameObject[] listPopUps;
+    private int comptPopUp;
+    public float timePopUp;
+    private GameObject google;
+    private bool boolGoogle;
+    private GameObject google2;
+    private bool boolGoogle2;
 
     // Use this for initialization
     void Start () {
         UnityAction[] tabFonctions = { functionButtonDemarrer, functionButtonPosteTravail, functionButtonInternet, functionButtonDiablo, //fonction du bureau
-                                        functionButtonPosteTravail, functionButtonInternet, functionButtonDiablo, functionButtonInvCommande, functionButtonTousProgs}; // fonction de demarrer
+                                        functionButtonPosteTravail, functionButtonInternet, functionButtonDiablo, functionButtonInvCommande, functionButtonTousProgs, // fonction de demarrer
+                                        functionFermer, functionButtonChance, functionButtonRecherche, // fonction de google1
+                                        functionFermer}; // fonction de google2
 
         listButtons = this.GetComponentsInChildren<Button>();
         for (int i = 0; i < listButtons.Length; i++)
@@ -23,9 +32,18 @@ public class WindowsButton : MonoBehaviour {
         }
         Debug.Log(listButtons.Length);
 
+        // Colomne Démarrer Initialisation
         colomneDemarrer = this.transform.Find("colomneDemarrer").gameObject;
         boolDemarrer = false;
         colomneDemarrer.SetActive(boolDemarrer);
+
+        // Google Initialisation
+        google = this.transform.Find("Google").gameObject;
+        boolGoogle = false;
+        google.SetActive(boolGoogle);
+        google2 = this.transform.Find("ChanceResult").gameObject;
+        boolGoogle2 = false;
+        google2.SetActive(boolGoogle2);
     }
 	
 	// Update is called once per frame
@@ -56,6 +74,39 @@ public class WindowsButton : MonoBehaviour {
     void functionButtonInternet()
     {
         Debug.Log("Test bouton internet réussi !");
+        if (!boolDemarrer)
+        {
+            boolDemarrer = true;
+            google.SetActive(boolDemarrer);
+        }
+        
+    }
+
+    void functionFermer()
+    {
+        boolDemarrer = false;
+        google.SetActive(boolGoogle);
+    }
+
+    void functionButtonChance()
+    {
+        boolGoogle2 = true;
+        google2.SetActive(boolGoogle2);
+    }
+
+    void functionButtonRecherche()
+    {
+        comptPopUp = 0;
+        Invoke("pupUps", 0);
+        Invoke("pupUps", timePopUp);
+        Invoke("pupUps", timePopUp * 2);
+        comptPopUp = 0;
+    }
+
+    void pupUps()
+    {
+        Instantiate(listPopUps[comptPopUp], this.transform);
+        comptPopUp++;
     }
 
     void functionButtonDiablo()
---
+++ GameJam2018/Assets/Scripts/closeWindows.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class closeWindows : MonoBehaviour {
+
+    public Button close;
+
+    // Use this for initialization
+    void Start () {
+        close.onClick.AddListener(functionFermer);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void functionFermer()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
