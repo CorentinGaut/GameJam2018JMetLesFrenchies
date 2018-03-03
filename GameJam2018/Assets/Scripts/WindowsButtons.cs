2556c8226ef09ed758cd5642988720190383a226
PC_STIEN\Justine <almes.justine@gmail.com>
2018-03-02 19:29:56 -0500

    commit test script

---
+++ GameJam2018/Assets/Scripts/WindowsButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsButtons : MonoBehaviour {
+
+    private ArrayList buttonsList;
+
+	// Use this for initialization
+	void Start () {
+		// Array des bouton children
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
