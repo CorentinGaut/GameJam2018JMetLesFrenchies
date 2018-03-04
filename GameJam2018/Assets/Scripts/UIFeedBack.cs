e38ede1dc2b7f151b7a43546fa6126720dcdd189
Gautier <corentin.chignin@hotmail.fr>
2018-03-04 18:26:35 +0100

    Ajout Feedback

---
+++ GameJam2018/Assets/Scripts/UIFeedBack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIFeedBack : MonoBehaviour {
+
+    public GameObject Composant;
+
+    int composantHP;
+    int composantHPMax;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        composantHP = Composant.GetComponent<BaseObject>().HP;
+        composantHPMax = Composant.GetComponent<BaseObject>().maxHP;
+
+        if (composantHP/composantHPMax != 1)
+        {
+            this.GetComponent<Image>().color = new Color(0.9f, 0.0f, 0.0f, 0.8f);
+        }
+        else
+        {
+            this.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f);
+        }
+        
+	}
+}
