d806ec5df14d149c53027187a2bf6a9b27182668
Lupusanghren <brisingr34130@gmail.com>
2018-03-04 04:51:57 -0500

    Ajout des sons uniquement si on repare

--- GameJam2018/Assets/Scripts/CharacterControler.cs
+++ GameJam2018/Assets/Scripts/CharacterControler.cs
@@ -78,7 +78,7 @@ public class CharacterControler : MonoBehaviour
 
         if (Input.GetButtonDown("Repare") && closestObject != null)
         {
-            kick.Play();
+            //kick.Play();
             anim.SetBool("isReparing", true);
             StartCoroutine(WaitAnim());
             closestObject.Repare();
